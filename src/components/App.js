6b8adc049c8501b2101cf57383d60aa12d66ead1
aman2222kumar <42084981+aman2222kumar@users.noreply.github.com>
2022-03-17 11:55:30 -0700

    added

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-import React from 'react'
-import '../styles/App.css';
-
+import React from "react";
+import "../styles/App.css";
 
 const App = () => {
-  
+  function HandleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-
-  )
-}
-
+    <>
+      <form onSubmit={HandleSubmit}>
+        <label>
+          Enter Name:
+          <input type="text" name="name"></input>
+        </label>
+        <br />
+        <label>
+          Enter Email:
+          <input type="email" name="email"></input>
+        </label>
+        <br />
+        <label>
+          Enter Password:
+          <input type="password" name="password"></input>
+        </label>
+        <br />
+        <label>
+          your consent:
+          <input type="checkbox" name="checkbox"></input>
+        </label>
+        <br />
+        <button>sign up</button>
+      </form>
+    </>
+  );
+};
 
 export default App;
